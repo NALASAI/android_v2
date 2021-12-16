2dbdc8757be2bc6cf5a353bc7828893c4eb2bf7d
kms1212 <kms1212g@gmail.com>
2021-12-16 17:23:57 +0900

    project init

---
+++ app/src/main/java/com/example/myapp_2/MainActivity.java
@@ -0,0 +1,14 @@
+0package com.example.myapp_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
